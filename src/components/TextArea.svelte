7fa24f021fd65cd0a04ca4331625d25f86343d09
Ezequiel <ezequiel.sanson@hotmail.com>
2023-02-07 13:28:41 -0300

    Proyecto Terminado
    Involucra Peticiones a una API de IA de lenguaje

---
+++ src/components/TextArea.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { checkIsEnglish } from "../services/ia";
+    import { isValidInput } from "./store";
+
+
+    const handleChange = async (e) => {
+        const { value } = e.target
+        const isValid = value.length > 0
+        if(!isValid) {
+            isValidInput.set(false)
+            return
+        }
+        
+        const isEnglish = await checkIsEnglish(value)
+        isValidInput.set(isEnglish)
+    }
+</script>
+
+<textarea 
+    on:input={handleChange}
+    class="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+    id="result" 
+    placeholder="Ingrese su frase" 
+    name="english" 
+    rows="5" 
+    cols="40"
+    >
+</textarea>
---
+++ src/components/store.js
@@ -0,0 +1,3 @@
+import {writable} from 'svelte/store'
+//Esto es un estado global en svelte
+export const isValidInput = writable(false)
